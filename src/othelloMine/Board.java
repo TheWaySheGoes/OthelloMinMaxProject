27a8ea7572ca379eabdef76a2fe597b2b6822ddc
Lukasz <lukasz.kurasinski@gmail.com>
2018-11-23 13:21:18 +0100

    working but with some bugs

--- src/othelloMine/Board.java
+++ src/othelloMine/Board.java
@@ -2,83 +2,116 @@ package othelloMine;
 
 import java.util.LinkedList;
 
-public class Board {
-	String[][] board = new String[4][4];
-	String player = "";
-	String position = "";
-	int boardLevel = 0;
-	int nbrOfTakenX = 0;
-	int nbrOfTakenO = 0;
+public class Board implements Comparable {
+	private String[][] board	  ={{".",".",".",".",".",".",".","."},
+									{".",".",".",".",".",".",".","."},
+									{".",".",".",".",".",".",".","."},
+									{".",".",".","X","O",".",".","."},
+									{".",".",".","O","X",".",".","."},
+									{".",".",".",".",".",".",".","."},
+									{".",".",".",".",".",".",".","."},
+									{".",".",".",".",".",".",".","."}};	// = new String[4][4];
+									
+		
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	private String player = "";
+	private String position = "";
+	private int level = 0;
+	private int nbrOfTakenX = 0;
+	private int nbrOfTakenO = 0;
+	private int value = 0;
+	private int boardLength = board.length;
+	private LinkedList<String> movehistory = new LinkedList<String>();
 
 	public Board() {
-		for (int i = 0; i < board.length; i++) {
-			for (int j = 0; j < board.length; j++) {
-				board[i][j] = ".";
-			}
-		}
+//		for (int i = 0; i < board.length; i++) {
+//			for (int j = 0; j < board.length; j++) {
+//				board[i][j] = ".";
+//			}
+//		}
 	}
 
 	public Board(String[][] b) {
 		this.board = b;
 	}
 
+	public Board(String[][] board,String player, String position, int boardLevel,int value, LinkedList<String> moveHist) {
+		this.board = board;
+		this.player = player;
+		this.position = position;
+		this.level = boardLevel;
+		this.value=value;
+		this.movehistory = moveHist;
+	}
+
+	public int getNbrOfTakenX() {
+		return nbrOfTakenX;
+	}
+
+	public int getNbrOfTakenO() {
+		return nbrOfTakenO;
+	}
+
 	/**
-	 * puts a pice on the board if possible otherwise returns false
+	 * puts a piece on the board if possible otherwise returns false
 	 * 
 	 * @param row
 	 * @param col
 	 * @param player
+	 *            "X" or "O"
 	 * @return
 	 */
 	public boolean insertValue(int row, int col, String player) {
 		try {
-			if (player.equals("X") || player.equals("O")) {
+			if ((player.equals("X") || player.equals("O")) && board[row][col].equals(".")&&this.isValidMove(row, col, player)) {
 				board[row][col] = player;
+				this.player = player;
+				level++;
+				movehistory.add(row + "," + col);
+				this.flipLeft(row, col, player, row, col);
+				this.flipRight(row, col, player, row, col);
+				this.flipUp(row, col, player, row, col);
+				this.flipDown(row, col, player, row, col);
+				this.flipUpLeft(row, col, player, row, col);
+				this.flipUpRight(row, col, player, row, col);
+				
+				this.sumUpBoard();
+	//			System.out.println(this.toString());
 				return true;
+			
 			}
 		} catch (Exception e) {
+			return false;
 		}
 		return false;
 	}
 
-	// benefit
-	public void checkNbrOfTaken() {
+	private void sumUpBoard() {
+		int tempX = 0;
+		int tempO = 0;
+
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board.length; j++) {
-				if(this.board[i][j].equals("O")) {
-					this.nbrOfTakenO++;
+				if (this.board[i][j].equals("O")) {
+					tempO++;
 				}
-				if(this.board[i][j].equals("X")) {
-					this.nbrOfTakenX++;
+				if (this.board[i][j].equals("X")) {
+					tempX++;
 				}
 			}
 		}
+		this.nbrOfTakenO = tempO;
+		this.nbrOfTakenX = tempX;
+		this.value = this.nbrOfTakenX - this.nbrOfTakenO;
 	}
-
-	public String checkWhoWinns() {
-		String temp = "";
-		if (this.isBoardFull()) {
-			int counter = 0;
-			for (int i = 0; i < board.length; i++) {
-				for (int j = 0; j < board.length; j++) {
-					if (board[i][j] == "X") {
-						counter++;
-					}
-				}
-			}
-			if (counter - 16 < 0) {
-				temp = "Computer wins";
-			} else if (counter - 16 > 0) {
-				temp = "User wins";
-			} else if (counter - 16 == 0) {
-				temp = "Its a draw";
-			}
-		}
-		return temp;
-	}
-
 	
-	
+	/**
+	 * 
+	 * @return
+	 */
 	public boolean isBoardFull() {
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board.length; j++) {
@@ -89,26 +122,429 @@ public class Board {
 		}
 		return true;
 	}
-
-	public void setBoardLevel(int bl) {
-		this.boardLevel = bl;
+	
+	public String checkWhoWinns() {
+		String temp = "";
+		if (this.isBoardFull()) {
+			if (this.nbrOfTakenO>this.nbrOfTakenX) {
+				temp = "O wins," + this.nbrOfTakenO;
+			} else if (this.nbrOfTakenO<this.nbrOfTakenX) {
+				temp = "X wins," + this.nbrOfTakenX;
+			} else {
+				temp = "Its a draw";
+			}
+		}
+		return temp;
 	}
 
+	public boolean hasPlayerValidMove(String player) {
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board.length; j++) {
+				return isValidMove(i,j,player);
+			}
+		}
+		return false;
+	}
+
+	
+	public void addMovehistory(int row,int col) {
+		this.movehistory.add(row+","+col);
+	}
+	
+	//Neighbor
+	private boolean isValidMove(int row,int col,String player) {
+		if(player.equals("X")) {
+			try {
+				if(this.board[row-1][col-1].equals("O")) {
+					return true;
+				}
+			}catch(Exception e) {}
+			try {
+				if(this.board[row-1][col].equals("O")) {
+					return true;
+				}
+			}catch(Exception e) {}
+			try {
+				if(this.board[row-1][col+1].equals("O")) {
+					return true;
+				}
+			}catch(Exception e) {}
+			try {
+				if(this.board[row][col-1].equals("O")) {
+					return true;
+				}
+			}catch(Exception e) {}
+			try {
+				if(this.board[row][col+1].equals("O")) {
+					return true;
+				}
+			}catch(Exception e) {}
+			try {
+				if(this.board[row+1][col-1].equals("O")) {
+					return true;
+				}
+			}catch(Exception e) {}
+			try {
+				if(this.board[row+1][col].equals("O")) {
+					return true;
+				}
+			}catch(Exception e) {}
+			try {
+				if(this.board[row+1][col+1].equals("O")) {
+					return true;
+				}
+			}catch(Exception e) {}
+			
+		}else if(player.equals("O")) {
+			try {
+				if(this.board[row-1][col-1].equals("X")) {
+					return true;
+				}
+			}catch(Exception e) {}
+			try {
+				if(this.board[row-1][col].equals("X")) {
+					return true;
+				}
+			}catch(Exception e) {}
+			try {
+				if(this.board[row-1][col+1].equals("X")) {
+					return true;
+				}
+			}catch(Exception e) {}
+			try {
+				if(this.board[row][col-1].equals("X")) {
+					return true;
+				}
+			}catch(Exception e) {}
+			try {
+				if(this.board[row][col+1].equals("X")) {
+					return true;
+				}
+			}catch(Exception e) {}
+			try {
+				if(this.board[row+1][col-1].equals("X")) {
+					return true;
+				}
+			}catch(Exception e) {}
+			try {
+				if(this.board[row+1][col].equals("X")) {
+					return true;
+				}
+			}catch(Exception e) {}
+			try {
+				if(this.board[row+1][col+1].equals("X")) {
+					return true;
+				}
+			}catch(Exception e) {}
+			
+		}
+		return false;
+		
+	}
+	
+
+	
+	private void flipLeft(int row, int col, String player, int endRow, int endCol) {
+		if (player.equals("X")) {
+			try {
+				if (this.board[endRow][endCol - 1].equals("O")) {
+					flipLeft(row, col, player, endRow, endCol - 1);
+				} else if (this.board[endRow][endCol - 1].equals("X")) {
+					for (int i = endCol-1; i <= col; i++) {
+						board[row][i] = "X";
+					}
+				} else if (this.board[row][col - 1].equals(".")) {
+					return;
+				}
+			} catch (Exception e) {
+			}
+		} else if (player.equals("O")) {
+			try {
+				if (this.board[endRow][endCol - 1].equals("X")) {
+					flipLeft(row, col, player, endRow, endCol - 1);
+				} else if (this.board[endRow][endCol - 1].equals("O")) {
+					for (int i = endCol-1; i <= col; i++) {
+						board[row][i] = "O";
+					}
+				} else if (this.board[row][col - 1].equals(".")) {
+					return;
+				}
+			} catch (Exception e) {
+			}
+		}
+	}
+
+	private void flipRight(int row, int col, String player, int endRow, int endCol) {
+		if (player.equals("X")) {
+			try {
+				if (this.board[endRow][endCol + 1].equals("O")) {
+					flipRight(row, col, player, endRow, endCol + 1);
+				} else if (this.board[endRow][endCol + 1].equals("X")) {
+					for (int i = col; i <= endCol+1; i++) {
+						board[row][i] = "X";
+					}
+				} else if (this.board[row][col + 1].equals(".")) {
+					return;
+				}
+			} catch (Exception e) {
+			}
+		} else if (player.equals("O")) {
+			try {
+				if (this.board[endRow][endCol + 1].equals("X")) {
+					flipRight(row, col, player, endRow, endCol + 1);
+				} else if (this.board[endRow][endCol + 1].equals("O")) {
+					for (int i = col; i <= endCol+1; i++) {
+						board[row][i] = "O";
+					}
+				} else if (this.board[row][col + 1].equals(".")) {
+					return;
+				}
+			} catch (Exception e) {
+			}
+		}
+	}
+	
+	private void flipUp(int row, int col, String player, int endRow, int endCol) {
+		if (player.equals("X")) {
+			try {
+				if (this.board[endRow-1][endCol].equals("O")) {
+					flipUp(row, col, player, endRow-1, endCol);
+				} else if (this.board[endRow-1][endCol].equals("X")) {
+					for (int i = endRow-1; i <= row; i++) {
+						board[i][col] = "X";
+					}
+				} else if (this.board[endRow-1][endCol].equals(".")) {
+					return;
+				}
+			} catch (Exception e) {
+			}
+		} else if (player.equals("O")) {
+			try {
+				if (this.board[endRow-1][endCol].equals("X")) {
+					flipUp(row, col, player, endRow-1, endCol);
+				} else if (this.board[endRow-1][endCol].equals("O")) {
+					for (int i = endRow-1; i <= row; i++) {
+						board[i][col] = "O";
+					}
+				} else if (this.board[endRow-1][endCol].equals(".")) {
+					return;
+				}
+			} catch (Exception e) {
+			}
+		}
+	}
+
+	private void flipDown(int row, int col, String player, int endRow, int endCol) {
+		if (player.equals("X")) {
+			try {
+				if (this.board[endRow+1][endCol].equals("O")) {
+					flipDown(row, col, player, endRow+1, endCol);
+				} else if (this.board[endRow+1][endCol].equals("X")) {
+					for (int i = row; i <= endRow+1; i++) {
+						board[i][col] = "X";
+					}
+				} else if (this.board[endRow+1][endCol].equals(".")) {
+					return;
+				}
+			} catch (Exception e) {
+			}
+		} else if (player.equals("O")) {
+			try {
+				if (this.board[endRow+1][endCol].equals("X")) {
+					flipDown(row, col, player, endRow+1, endCol);
+				} else if (this.board[endRow+1][endCol].equals("O")) {
+					for (int i = row; i <= endRow+1; i++) {
+						board[i][col] = "O";
+					}
+				} else if (this.board[endRow+1][endCol].equals(".")) {
+					return;
+				}
+			} catch (Exception e) {
+			}
+		}
+	}
+	
+	
+	private void flipUpLeft(int row, int col, String player, int endRow, int endCol) {
+		if (player.equals("X")) {
+			try {
+				if (this.board[endRow-1][endCol-1].equals("O")) {
+					flipUpLeft(row, col, player, endRow-1, endCol-1);
+				} else if (this.board[endRow-1][endCol-1].equals("X")) {
+					for (int i = row; i >= endRow-1; i--) {
+						board[i][col] = "X";
+						col--;
+					}
+				} else if (this.board[endRow-1][endCol-1].equals(".")) {
+					return;
+				}
+			} catch (Exception e) {
+			}
+		} else if (player.equals("O")) {
+			try {
+				if (this.board[endRow-1][endCol-1].equals("X")) {
+					flipUpLeft(row, col, player, endRow-1, endCol-1);
+				} else if (this.board[endRow-1][endCol-1].equals("O")) {
+					for (int i = row; i >= endRow-1; i++) {
+						board[i][col] = "O";
+						col--;
+					}
+				} else if (this.board[endRow-1][endCol-1].equals(".")) {
+					return;
+				}
+			} catch (Exception e) {
+			}
+		}
+	}
+	private void flipUpRight(int row, int col, String player, int endRow, int endCol) {
+		if (player.equals("X")) {
+			try {
+				if (this.board[endRow-1][endCol+1].equals("O")) {
+					flipUpRight(row, col, player, endRow-1, endCol+1);
+				} else if (this.board[endRow-1][endCol+1].equals("X")) {
+					for (int i = row; i >= endRow-1; i--) {
+						board[i][col] = "X";
+						col++;
+					}
+				} else if (this.board[endRow-1][endCol+1].equals(".")) {
+					return;
+				}
+			} catch (Exception e) {
+			}
+		} else if (player.equals("O")) {
+			try {
+				if (this.board[endRow-1][endCol+1].equals("X")) {
+					flipUpRight(row, col, player, endRow-1, endCol+1);
+				} else if (this.board[endRow-1][endCol+1].equals("O")) {
+					for (int i = row+1; i >= endRow-1; i--) {
+						board[i][col] = "O";
+						col++;
+					}
+				} else if (this.board[endRow-1][endCol+1].equals(".")) {
+					return;
+				}
+			} catch (Exception e) {
+			}
+		}
+	}
+
+	
+	
+
+	/**
+	 * value tested at terminal node.
+	 * 
+	 * @return
+	 */
+	public int getValue() {
+		return this.value;
+	}
+
+	public String getPlayer() {
+		return player;
+	}
+
+	public void setPlayer(String player) {
+		this.player = player;
+	}
+
+	public String getPosition() {
+		return position;
+	}
+
+	public void setPosition(String position) {
+		this.position = position;
+	}
+
+	public int getLevel() {
+		return level;
+	}
+
+	public void setBoardLevel(int bl) {
+		this.level = bl;
+	}
+
+	public int getBoardLength() {
+		return boardLength;
+	}
+
+	public String[][] getBoard() {
+		return this.board;
+	}
+
+	public String[][] getBoardCopy() {
+		String[][] temp = new String[this.boardLength][this.boardLength];
+		for (int i = 0; i < temp.length; i++) {
+			for (int j = 0; j < temp.length; j++) {
+				temp[i][j] = this.board[i][j];
+			}
+		}
+		return temp;
+	}
+	public LinkedList<String> getMoveHistoryCopy() {
+		LinkedList<String> temp = new LinkedList<String>();
+		for (int i = 0; i < movehistory.size(); i++) {	
+				temp.add(this.movehistory.get(i));
+		}
+		return temp;
+	}
+	public Board cloneThisBoardObj() {
+		return new Board(this.getBoardCopy(),this.player,this.position,this.level,this.value,this.getMoveHistoryCopy());
+	}
+
+	public LinkedList<String> getMovehistory() {
+		return movehistory;
+	}
+
+	private String showMoveHistory() {
+		String temp="";
+		for (int i = 0; i < this.movehistory.size(); i++) {
+			temp+=this.movehistory.get(i)+";";
+		}
+		return temp;
+	}
+	
 	public String toString() {
 		String temp = "";
 		for (int i = 0; i < board.length; i++) {
+			temp+=i+"";
 			for (int j = 0; j < board.length; j++) {
+				
 				temp += "|" + board[i][j] + "|";
 			}
 			temp += "\n";
+			if(i==board.length-1) {
+				for (int j = 0; j < board.length; j++) {
+					temp+="  "+j;
+				}
+				temp+="\n";
+			}
 		}
-		this.checkNbrOfTaken();
-		
-		
-		temp += "boardLevel:" + this.boardLevel + "\n";
+		temp += "boardLevel:" + this.level + "\n";
+		temp += "boardValue:" + this.value + "\n";
+		temp += "boardPlayer:" + this.player + "\n";
 		temp += "numberOfTakenX:" + this.nbrOfTakenX + "\n";
-		temp += "numberOfTakenY:" + this.nbrOfTakenO+ "\n";
+		temp += "numberOfTakenO:" + this.nbrOfTakenO + "\n";
+		temp += "move history:" + this.showMoveHistory() + "\n";
 		
+
 		return temp;
 	}
+
+	public void setLevel(int level) {
+		this.level = level;
+	}
+
+	/**
+	 * Sorts according to board overall value (utilitie)
+	 */
+	@Override
+	public int compareTo(Object obj) {
+		Board b = (Board) obj;
+		return this.getValue()-b.getValue();
+
+	}
+
+	
+
 }
--- src/othelloMine/OthelloMain.java
+++ src/othelloMine/OthelloMain.java
@@ -1,53 +1,119 @@
 package othelloMine;
 
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class OthelloMain {
-	Board board = new Board();
-	
-	
-	public void evalBoard(Board board) {
-		
-	}
-	
-	
+	private Board board = new Board();
+	private LinkedList<Board> terminalStateBoards = new LinkedList<Board>();
+	private int evalDepth = 5;
+	private int move = 0;
+	private Board optimalTerminalBoard = board;
+	private boolean gameIsRunning = true;
+	private long startTime;// for time testing
 	
 	public void runGame() {
+		
 		System.out.println(board.toString());
-		while (true) {
+		
+		while (gameIsRunning) {
 			try {
-				//first move player X
+				
+				// first move player X
+				// get input from console
 				Scanner scanner = new Scanner(System.in);
-				System.out.println("enter cell x,y");
+				System.out.println("enter cell x,y --if bad input game crashes!--");
 				String[] input = scanner.nextLine().split(",");
-				board.insertValue(Integer.parseInt(input[0]), Integer.parseInt(input[1]), "X");
-			//	String[] oponent= checkGame(board).split(",");
-			//	board.insertValue(Integer.parseInt(oponent[0]), Integer.parseInt(oponent[1]), "O");
+				// try to put a move on a board if possible
+				if (board.insertValue(Integer.parseInt(input[0]), Integer.parseInt(input[1]), "X")) {
+					// board.addMovehistory(Integer.parseInt(input[0]), Integer.parseInt(input[1]));
+					System.out.println("********YOUR MOVE************* \n" + board.toString());
+					move++;
+				}
+				//for time testing
+				startTime=System.currentTimeMillis();
 				
+				// computer move player O
+				// evaluate all of the possible moves up to depth +3
+				this.evaluate(board, board.getLevel() + evalDepth);
 				
-				System.out.println(board.toString());
+				//for time testing
+				System.out.println("evaluation with depth "+evalDepth+" took:"+(System.currentTimeMillis()-startTime)+" millis.");
+				
+				// try to use the move from evaluation
+				try {
+					String[] oponent = optimalTerminalBoard.getMovehistory().get(move).split(",");
+					if (oponent.length != 0) {
+						if (board.insertValue(Integer.parseInt(oponent[0]), Integer.parseInt(oponent[1]), "O")) {
+							System.out.println("oponent choose position: " + oponent[0] + "," + oponent[1]);
+							System.out.println("********OPONENT MOVE************* \n" + board.toString());
+							move++;
+						}
+					}
+				} catch (Exception e) {
+				}
+
+				// check if theres a winner (fulll board)
+				if (board.isBoardFull()) {
+					System.out.println(board.checkWhoWinns());
+					gameIsRunning = false;
+				}
+
+				// this board is going to be replaced under evaluation
+				optimalTerminalBoard = new Board();
+				optimalTerminalBoard.setValue((int) Math.pow(evalDepth, Integer.MAX_VALUE));
+
+				// this is just for JVM and OS
+				Thread.sleep(50);
 			} catch (Exception e) {
 
 				e.printStackTrace();
 			}
 		}
 	}
-	
-	
-	
-	public void eval(Board board) {
-		
-		//1. termina state(check before move from scanner)
-		//2. loop throu possible moves choose best one for a give player + recursive
-		
-	}
-	
 
-	
-	
-	
+	public void evaluate(Board initialStateBoard, int depth) {
+
+		// terminal test
+		if (initialStateBoard.isBoardFull() || initialStateBoard.getLevel() >= depth) {
+			// utility function replacing optimal board if this better
+			if (initialStateBoard.getValue() <= optimalTerminalBoard.getValue()) {
+				optimalTerminalBoard = initialStateBoard.cloneThisBoardObj();
+				// System.out.println("newest optimal terminal board: \n" +
+				// optimalTerminalBoard.toString());
+			}
+		} else {
+
+			// generating actions
+			LinkedList<Board> actionsOflegalMovesInTheState = new LinkedList<Board>();
+			for (int i = 0; i < initialStateBoard.getBoardLength(); i++) {
+				for (int j = 0; j < initialStateBoard.getBoardLength(); j++) {
+					Board temp = initialStateBoard.cloneThisBoardObj();
+					// adding possible actions to list
+					if (initialStateBoard.getPlayer().equals("X")) {
+						if (temp.insertValue(i, j, "O")) {
+							actionsOflegalMovesInTheState.add(temp.cloneThisBoardObj());
+						}
+					} else if (initialStateBoard.getPlayer().equals("O")) {
+						if (temp.insertValue(i, j, "X")) {
+							actionsOflegalMovesInTheState.add(temp.cloneThisBoardObj());
+						}
+					}
+				}
+			}
+			// recursing all legal actions for evaluation again
+			// System.out.println("legal actions list size:
+			// "+actionsOflegalMovesInTheState.size());
+			for (int i = 0; i < actionsOflegalMovesInTheState.size(); i++) {
+				// System.out.println("legal action board \n" +
+				// actionsOflegalMovesInTheState.get(i).toString());
+				evaluate(actionsOflegalMovesInTheState.get(i), depth);
+			}
+		}
+	}
+
 	public static void main(String[] args) {
-		OthelloMain om = new OthelloMain();
-		om.runGame();
+		OthelloMain o = new OthelloMain();
+		o.runGame();
 	}
 }
